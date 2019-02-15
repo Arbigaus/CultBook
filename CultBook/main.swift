0e285eef0d1251d6af86b253422d0a7441e5b14b
Gerson Arbigaus <gerson87@gmail.com>
2019-02-15 21:19:22 -0200

    Initial Commit

---
+++ CultBook/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CultBook
+//
+//  Created by Gerson Arbrugaus on 15/02/19.
+//  Copyright © 2019 Arbigaus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
